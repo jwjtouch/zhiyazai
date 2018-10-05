b79e5f7f715ec896bc3a131b59e429cc5512f12c
jiaoweijia <907306715@qq.com>
2018-10-05 21:20:47 +0800

    验证码

--- application/mydream/controller/Login.php
+++ application/mydream/controller/Login.php
@@ -2,6 +2,7 @@
 namespace app\mydream\controller;
 
 use think\Controller;
+use think\captcha\Captcha;
 
 class Login extends Controller
 {
@@ -11,8 +12,24 @@ class Login extends Controller
         return $this->fetch('index',['title'=>'登录页面']);
     }
 
-    public function hello($name = 'ThinkPHP5')
+    //TODO 未实现
+    public function register()
     {
-        return 'hello,' . $name;
+        $param = Request::instance()->param();
+        $param['pwdsalt'] = substr(md5(uniqid('weteam')), 0, 8);
+        $param['password'] = md5($param['password'].$param['pwdsalt']);
+        $param['create_time'] = time();
+        $param['status'] = 1;
+        if (Db::name('user')->insert($param)) {
+            $this->success('注册成功！', config('default_login'), '', 2);
+        } else {
+            $this->error('注册失败');
+        }
+    }
+
+    //获取验证码
+    public function getcaptcha(){
+        $captcha = new Captcha(config('captcha.'));
+        return $captcha->entry();
     }
 }
---
+++ data/zyz_admin.sql
@@ -0,0 +1,33 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : erp系统
+Source Server Version : 50505
+Source Host           : 192.168.9.4:3306
+Source Database       : shi
+
+Target Server Type    : MYSQL
+Target Server Version : 50505
+File Encoding         : 65001
+
+Date: 2018-09-30 10:22:04
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for zyz_admin
+-- ----------------------------
+CREATE TABLE `zyz_admin` (
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `name` varchar(30) DEFAULT '' COMMENT '姓名',
+  `account` varchar(30) DEFAULT '' COMMENT '账号',
+  `pwd` varchar(50) DEFAULT '' COMMENT '密码',
+  `salt` varchar(10) DEFAULT '' COMMENT '密码盐',
+  `create_time` int(10) DEFAULT NULL COMMENT '添加时间',
+  `update_time` int(10) DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  KEY `account` (`account`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='管理员表';
+
+alter table zyz_admin add salt varchar(10) DEFAULT '' commit '密码盐';
